21f8f97851d5004375d580a32d50ca05140d6422
VAIBHAV DEEPAK SHINDE <123886126+vaibhavshinde8@users.noreply.github.com>
2024-09-25 20:19:35 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,7 +46,7 @@ function NavBar() {
               fontSize: '28px',
             }}
           >
-            Mohit Sojitra
+            Vaibhav Shinde
           </p>
         </Navbar.Brand>
         <Navbar.Toggle
@@ -80,17 +80,6 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen style={{marginBottom: '2px'}} />{' '}
-                Projects
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
                 to="/resume"
                 onClick={() => updateExpanded(false)}
               >
@@ -98,19 +87,9 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                href="https://blog.mohitsojitra.tech/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{marginBottom: '2px'}} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/MohitSojitra/portfolio"
+                href="https://github.com/vaibhavshinde8"
                 target="_blank"
                 className="fork-btn-inner"
               >
