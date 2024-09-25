e1f7d7fc4e1e817a067d5081ca32366d8004290a
VAIBHAV DEEPAK SHINDE <123886126+vaibhavshinde8@users.noreply.github.com>
2024-09-25 19:51:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/Resume";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -35,7 +34,6 @@ function App() {
         <ScrollToTop />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/project" component={Projects} />
           <Route path="/about" component={About} />
           <Route path="/resume" component={Resume} />
         </Switch>
