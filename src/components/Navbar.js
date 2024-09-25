d3f10a760951030e82789086b50f2ad2c267e9ce
VAIBHAV DEEPAK SHINDE <123886126+vaibhavshinde8@users.noreply.github.com>
2024-09-25 20:30:00 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -83,7 +83,7 @@ function NavBar() {
                 to="/resume"
                 onClick={() => updateExpanded(false)}
               >
-                <CgFileDocument style={{marginBottom: '2px'}} /> Resume
+                <CgFileDocument style={{marginBottom: '2px'}} /> Education
               </Nav.Link>
             </Nav.Item>
 
