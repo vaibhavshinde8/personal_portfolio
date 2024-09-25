1b4fe26c7ca512ce15c59929ac40be9b1f8683e0
VAIBHAV DEEPAK SHINDE <123886126+vaibhavshinde8@users.noreply.github.com>
2024-09-25 19:59:10 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -27,7 +27,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> MOHIT SOJITRA</strong>
+                <strong className="main-name"> VAIBHAV SHINDE</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
